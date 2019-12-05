905a7026724125dea22df2ec4917f7de9b697635
Bouteiller Alan <bouteiller.alan@gmail.com>
2019-12-05 17:41:43 +0100

    :sparkles: feat(env): add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {ConfigModule} from './config/config.module';
+import {ConfigService} from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (env: ConfigService) => ({
+        uri: `mongodb://${env.get('db_user')}:${env.get('db_pass')}@${env.get('db_uri')}:${env.get('db_port')}/${env.get('db_name')}`,
+        useNewUrlParser: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
